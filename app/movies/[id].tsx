027baff358b313a2ab9b7f56ac92f3710381b47f
Arnav S Rao <arnavsrao09@gmail.com>
2025-11-23 21:33:10 +0530

    Tab ui update

--- app/movies/[id].tsx
+++ app/movies/[id].tsx
@@ -1,12 +1,14 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useLocalSearchParams } from 'expo-router';
 
-const MovieDetails = () => {
+const Details = () => {
+  const { id } = useLocalSearchParams();
   return (
     <View>
-      <Text>MovieDetails</Text>
+      <Text>Movie Details: {id}</Text>
     </View>
   )
 }
 
-export default MovieDetails
+export default Details
