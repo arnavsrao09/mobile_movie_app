d138304407dd62d2574d48ef236eeaf1f1efa753
Arnav S Rao <arnavsrao09@gmail.com>
2025-11-25 17:30:45 +0530

    Add search func to home page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,24 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { Image, ScrollView, View } from "react-native";
+import SearchBar from "@/components/SearchBar";
+import { useRouter } from "expo-router";
 
 export default function Index() {
+  const router = useRouter();
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-5xl text-dark-200" >Welcome!</Text>
+    <View className="flex-1 items-center justify-center bg-primary">
+      <Image source={images.bg} className="w-full h-full absolute" />
+      <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{ minHeight: '100%', paddingBottom: 5 }}>
+        <Image source={icons.logo} className="mt-20 mb-5 mx-auto w-10 h-10 " />
+
+        <View className="flex-1 mt-5">
+          <SearchBar 
+          onPress={() => router.push("/search")}
+          placeholder="Search for Movies"
+          />
+        </View>
+      </ScrollView>
     </View>
   );
 }
